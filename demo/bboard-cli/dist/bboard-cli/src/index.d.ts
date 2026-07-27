40f766de77ed60106934c3c3d85ae612c6629361
amisayhan88 <amisayhan88@users.noreply.github.com>
2026-07-27 19:46:42 +0530

    refactor: flatten repository structure to make all modules directly accessible at root level

--- demo/bboard-cli/dist/bboard-cli/src/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { type BBoardProviders } from '../../api/src/index';
-import { type Ledger } from '../../contract/src/managed/bboard/contract/index.js';
-import { type Logger } from 'pino';
-import { type Config } from './config.js';
-import { type ContractAddress } from '@midnight-ntwrk/midnight-js-protocol/compact-runtime';
-import { TestEnvironment } from '@midnight-ntwrk/testkit-js';
-export declare const getBBoardLedgerState: (providers: BBoardProviders, contractAddress: ContractAddress) => Promise<Ledger | null>;
-export declare const run: (config: Config, testEnv: TestEnvironment, logger: Logger) => Promise<void>;
